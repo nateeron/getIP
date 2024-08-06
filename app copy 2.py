aedbc6e59fcf5a78256aafe6f1c07ba8d5dbb1a6
cayoshi <nateeronron@gmail.com>
2024-08-06 15:13:17 +0700

    asdasd

--- app copy 2.py
+++ app copy 2.py
@@ -11,5 +11,14 @@ def index():
         client_ip = request.remote_addr
     return f'Your IP address is: {client_ip}'
 
+@app.route('/')
+def indexs():
+    # Get the client IP address
+    if request.headers.getlist("X-Forwarded-For"):
+        client_ip = request.headers.getlist("X-Forwarded-For")[0]
+    else:
+        client_ip = request.remote_addr
+    return f'Your IP address is: {client_ip}'
+
 if __name__ == '__main__':
     app.run(debug=True)
--- app copy.py
+++ app copy.py
@@ -1,52 +1,36 @@
-from flask import Flask, request, jsonify, redirect, url_for
-import requests
-from flask_cors import CORS
+from flask import Flask, request, jsonify, render_template
+import geoip2.database
 
 app = Flask(__name__)
-CORS(app) 
+reader = geoip2.database.Reader('path/to/GeoLite2-City.mmdb')  # Replace with your database path
 
-def get_location_from_ip(ip_address):
-    try:
-        response = requests.get(f'http://ip-api.com/json/{ip_address}')
-        data = response.json()
-
-        if data['status'] == 'success':
-            location_info = {
-                'country': data['country'],
-                'region': data['regionName'],
-                'city': data['city'],
-                'zip': data['zip'],
-                'lat': data['lat'],
-                'lon': data['lon'],
-                'timezone': data['timezone'],
-                'isp': data['isp'],
-                'org': data['org'],
-                'as': data['as']
-            }
-            return location_info
-        else:
-            return {'error': 'Failed to get location information'}
-
-    except Exception as e:
-        return {'error': str(e)}
-
-@app.route('/', methods=['GET'])
-def run():
-  
-    return "OK Run..."
-
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/get', methods=['GET'])
 def get_ip_location():
-        # Get the client IP address
+    # Get the client IP address
     if request.headers.getlist("X-Forwarded-For"):
         client_ip = request.headers.getlist("X-Forwarded-For")[0]
     else:
         client_ip = request.remote_addr
-  
-    location = get_location_from_ip(client_ip)
-    return jsonify(location)
+    
+    # Get location from IP using GeoIP2 database
+    try:
+        response = reader.city(client_ip)
+        city = response.city.name
+        country = response.country.name
+        latitude = response.location.latitude
+        longitude = response.location.longitude
+        location = f'{city}, {country} ({latitude}, {longitude})'
+    except geoip2.errors.AddressNotFoundError:
+        location = 'Location not found'
+
+    # Construct Google Maps URL
+    google_maps_url = f'https://www.google.com/maps/search/?api=1&query={latitude},{longitude}'
+
+    return jsonify({'location': location, 'google_maps_url': google_maps_url})
 
 if __name__ == '__main__':
     app.run(debug=True)
-
